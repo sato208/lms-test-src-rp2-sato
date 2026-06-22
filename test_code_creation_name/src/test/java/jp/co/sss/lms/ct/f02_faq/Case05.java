21fe2ab2b499309f3e29d4ebc84ea962a470d567
ysatou <ysatou@PC11040N>
2026-06-22 14:15:03 +0900

    add:佐藤嘉俊 ケース５、ケース６の追加
    ケース５よくある質問「キーワード検索」正常系確認テストの作成
    ケース６よくある質問「カテゴリ検索」正常系確認テストの作成

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case05.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case05.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.openqa.selenium.By;
 
-
 /**
  * 結合テスト よくある質問機能
  * ケース05
@@ -38,41 +37,160 @@ public class Case05 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		/** URL取得 */
+		String targetUrl = "http://localhost:8080/lms/";
+		/** URL開く */
+		goTo(targetUrl);
+		/** エビデンスキャプチャ取得 */
+		getEvidence(new Object() {
+		});
+		/** ログイン画面の判定1 */
+		String actualText = "ログイン";
+		String headingText = webDriver.findElement(By.tagName("h2")).getText();
+		assertEquals(actualText, headingText, "ログイン画面ではありません、またはログイン画面が正しく表示されていません");
+
+		/** ログイン画面の判定2 */
+		String actualButtonText = webDriver.findElement(By.className("btn-primary")).getAttribute("value");
+		assertEquals(actualText, actualButtonText, "ログイン画面ではありません、またはログイン画面が正しく表示されていません");
+
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		/**loginId,password欄にStudentAA01を入力 */
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA01");
+
+		webDriver.findElement(By.id("password")).sendKeys("StudentAA01");
+
+		/** エビデンスキャプチャ取得 */
+		getEvidence(new Object() {
+		}, "input");
+		/** ログインボタンを押下 */
+		webDriver.findElement(By.className("btn-primary")).click();
+
+		/** コース詳細画面判定1*/
+		String expectedUrl = "http://localhost:8080/lms/course/detail";
+		assertEquals(expectedUrl, webDriver.getCurrentUrl(), "ログイン成功後の画面URLが一致しません");
+
+		/** コース詳細画面判定2*/
+		String actualBreadcrumb = webDriver.findElement(By.className("breadcrumb")).findElement(By.tagName("li"))
+				.getText();
+		assertEquals("コース詳細", actualBreadcrumb, "コース詳細画面ではありません、またはコース詳細画面が正しく表示されていません");
+
+		/** エビデンスキャプチャ取得 */
+		getEvidence(new Object() {
+		}, "result");
+
 	}
-	
+
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		// TODO ここに追加
+
+		/** 上部メニュー機能を開く */
+		webDriver.findElement(By.className("dropdown-toggle")).click();
+		//		webDriver.findElement(By.linkText("機能")).click();
+
+		/** メニューが開いたら、中にある「ヘルプ」クリック*/
+		webDriver.findElement(By.xpath("//a[@href='/lms/help']")).click();
+		//		webDriver.findElement(By.className("dropdown-menu")).findElement(By.linkText("ヘルプ")).click();
+
+		/** ヘルプ画面判定1*/
+		String expectedHelpUrl = "http://localhost:8080/lms/help";
+		assertEquals(expectedHelpUrl, webDriver.getCurrentUrl(), "ヘルプ画面のURLが一致しません");
+
+		/** ヘルプ画面判定2*/
+		String actualHelpTitle = webDriver.findElement(By.tagName("h2")).getText();
+		assertEquals("ヘルプ", actualHelpTitle, "ヘルプ画面のタイトルが正しく表示されていません、またはヘルプ画面が正しく表示されていません");
+
+		/** エビデンスキャプチャ取得 */
+		getEvidence(new Object() {
+		});
+
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// TODO ここに追加
+
+		// 1. 現在のタブのIDをメモしておく
+		String oldWindow = webDriver.getWindowHandle();
+
+		// 2. 「よくある質問」リンク文字をクリック（別タブが開く）
+		webDriver.findElement(By.linkText("よくある質問")).click();
+
+		// 3. 今開いてるタブとは違うタブを開く
+		for (String windowHandle : webDriver.getWindowHandles()) {
+			if (!oldWindow.contentEquals(windowHandle)) {
+				webDriver.switchTo().window(windowHandle);
+				break;
+			}
+		}
+
+		/** よくある質問画面判定1*/
+		String expectedFaqUrl = "http://localhost:8080/lms/faq";
+		assertEquals(expectedFaqUrl, webDriver.getCurrentUrl(), "よくある質問画面のURLが一致しません");
+
+		/** よくある質問画面判定2*/
+		String actualFaqTitle = webDriver.findElement(By.tagName("h2")).getText();
+		assertEquals("よくある質問", actualFaqTitle, "よくある質問画面ではありません、またはよくある質問画面が正しく表示されていません");
+
+		/** エビデンスキャプチャ（結果）取得 */
+		getEvidence(new Object() {
+		});
+
 	}
+
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 キーワード検索で該当キーワードを含む検索結果だけ表示")
 	void test05() {
-		// TODO ここに追加
+		
+		/**キーワード欄にキャンセルを入力 */
+		webDriver.findElement(By.id("form")).sendKeys("キャンセル");
+		
+		/**検索押下 */
+		webDriver.findElement(By.cssSelector("input[value='検索']")).click();
+		
+		/**下までスクロール（確実に撮るため待機時間の確保）*/
+		try {
+	        Thread.sleep(3000); 
+	    } catch (InterruptedException e) {
+	        e.printStackTrace();
+	    }
+		
+		scrollTo("document.body.scrollHeight");
+		
+		try {
+	        Thread.sleep(3000); 
+	    } catch (InterruptedException e) {
+	        e.printStackTrace();
+	    }
+		
+		/** エビデンスキャプチャ（結果）取得 */
+		getEvidence(new Object() {
+		});
+		
 	}
-	
+
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 「クリア」ボタン押下で入力したキーワードを消去")
 	void test06() {
-		// TODO ここに追加
+		
+		/**クリア押下 */
+		webDriver.findElement(By.cssSelector("input[value='クリア']")).click();
+		
+		/** エビデンスキャプチャ（結果）取得 */
+		getEvidence(new Object() {
+		});
+		
 	}
 
 }
