18b8bccb3f2433a987cdab63accbfe231616bae1
ysatou <ysatou@PC11040N>
2026-06-25 16:48:14 +0900

    add:佐藤嘉俊 ケース15、ケース16追加
    ケース15受講生 初回ログイン利用規約に不同意のテストケース作成
    ケース16受講生 初回ログイン パスワード変更 入力チェックテストケース作成

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f06_login2/Case15.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f06_login2/Case15.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f06_login2;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト ログイン機能②
@@ -35,21 +37,72 @@ public class Case15 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		/** URL取得 */
+		String targetUrl = "http://localhost:8080/lms/";
+		/** URL開く */
+		goTo(targetUrl);
+		/** エビデンスキャプチャ取得 */
+		getEvidence(new Object() {
+		});
+		/** ログイン画面の判定1 */
+		String actualText = "ログイン";
+		String headingText = webDriver.findElement(By.tagName("h2")).getText();
+		assertEquals(actualText, headingText, "ログイン画面ではありません、またはログイン画面が正しく表示されていません");
+
+		/** ログイン画面の判定2 */
+		String actualButtonText = webDriver.findElement(By.className("btn-primary")).getAttribute("value");
+		assertEquals(actualText, actualButtonText, "ログイン画面ではありません、またはログイン画面が正しく表示されていません");
+
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに初期登録された未ログインの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		/**loginId,password欄にStudentAA03を入力 */
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA03");
+
+		webDriver.findElement(By.id("password")).sendKeys("StudentAA03");
+
+		/** エビデンスキャプチャ取得 */
+		getEvidence(new Object() {
+		}, "input");
+
+		/** ログインボタンを押下 */
+		webDriver.findElement(By.className("btn-primary")).click();
+
+		/** エビデンスキャプチャ取得 */
+		getEvidence(new Object() {
+		}, "result");
+
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 「同意します」チェックボックスにチェックをせず「次へ」ボタンを押下")
 	void test03() {
-		// TODO ここに追加
+
+		/** 次へボタンクリック*/
+		webDriver.findElement(By.xpath("//form[contains(@action, 'agreeSecurity')]//button[text()='次へ']")).click();
+
+		visibilityTimeout(By.className("error"), 5);
+
+		/**エラーメッセージ判定*/
+		String actualMessage = webDriver.findElement(By.className("error")).getText();
+		assertTrue(actualMessage.contains("セキュリティ規約への同意は必須です。"), "エラーメッセージが正しく表示されていません。実際の表示: " + actualMessage);
+
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		/** エビデンスキャプチャ取得 */
+		getEvidence(new Object() {
+		}, "result");
+
 	}
 
 }
